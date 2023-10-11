24473aa3415e87b30586c7aee0455168167158fd
likedevs <likemediamd8@gmail.com>
2023-10-11 18:55:53 +0300

    backend| swager: Setup Swager

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("BlockAids API")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/", app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true}));
+
+  app.enableCors({
+    origin: true,
+    allowedHeaders:
+      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization, Access-Control-Allow-Origin",
+    methods: "GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+  });
+
+  await app.listen(4000);
 }
+
 bootstrap();
