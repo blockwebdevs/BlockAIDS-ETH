3df8592d4fec6474103b428abbd688ddd1c7db67
likedevs <likemediamd8@gmail.com>
2023-10-11 18:55:53 +0300

    backend| swager: Setup Swager

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,55 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import * as path from "path";
+import * as process from "process";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from "./modules/users/users.module";
+import { User } from "./modules/users/entities/user.entity";
+import { FilesModule } from "./common/files/files.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { OrganizationsModule } from "./modules/organizations/organizations.module";
+import { Organization } from "./modules/organizations/entities/organization.entity";
+import { SpecialistsModule } from "./modules/specialists/specialists.module";
+import { Specialist } from "./modules/specialists/entities/specialist.entity";
+import { NotificationsModule } from "./modules/notifications/notifications.module";
+import { Notification } from "./modules/notifications/entities/notification.entity";
+import { TasksModule } from "./modules/tasks/tasks.module";
+import { TaskType } from "./modules/tasks/entities/task-type.entity";
+import { Task } from "./modules/tasks/entities/task.entity";
+import { AuthModule } from "./modules/auth/auth.module";
+import { BlockchainModule } from "./modules/blockchain/blockchain.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: "./.env",
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: Number(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      entities: [User, Organization, Specialist, Notification, TaskType, Task],
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, "../uploads"),
+    }),
+    FilesModule,
+    UsersModule,
+    OrganizationsModule,
+    SpecialistsModule,
+    NotificationsModule,
+    TasksModule,
+    AuthModule,
+    BlockchainModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
